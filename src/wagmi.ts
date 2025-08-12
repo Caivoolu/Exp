e0225f25bafe8874f2258aeb7a4d8a9a087daa46
Caivoolu <161195665+Caivoolu@users.noreply.github.com>
2025-08-13 04:49:16 +0700

    Add files via upload

---
+++ src/wagmi.ts
@@ -0,0 +1,23 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'Rains',
+  projectId: 'c5df0951573e834c17ba8d61110b38bd',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: true,
+});
