275e183dc495ecb5837a66dd325939e2ac2d6bf9
Caivoolu <161195665+Caivoolu@users.noreply.github.com>
2025-08-14 06:00:25 +0700

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,45 +1,34 @@
+// pages/_app.tsx
 import '../styles/global.css';
+import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider, type Locale } from '@rainbow-me/rainbowkit';
+import { config } from '../wagmi';
 import Sidebar from '../components/Sidebar';
-import Content from '../components/Content';
-import Home from './home';
-import Tool from './tool';
-import ShoppingCart from './shopping-cart';
-import DollarSign from './dollar-sign';
-import Info from './info';
-import User from './user';
+import { useState } from 'react';
 
-const PAGES = {
-  home: <Home />,
-  lab: <Tool />,
-  market: <ShoppingCart />,
-  games: <DollarSign />,
-  about: <Info />,
-  profile: <User />, // Content profil akan menampilkan wallet info
-};
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [active, setActive] = useState<keyof typeof PAGES>('home');
-
-  // Feather icons
-  useEffect(() => {
-    if (!document.querySelector('script#feather-icons')) {
-      const script = document.createElement('script');
-      script.id = 'feather-icons';
-      script.src = 'https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js';
-      script.async = true;
-      document.body.appendChild(script);
-    } else if ((window as any).feather) {
-      (window as any).feather.replace();
-    }
-  }, [active]);
+  const { locale } = useRouter() as { locale: Locale };
+  const [activePage, setActivePage] = useState('home');
 
   return (
-    <div className="appWrapper">
-      <Sidebar active={active} setActive={setActive} />
-      <Content active={active} pages={PAGES} />
-    </div>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale={locale}>
+          <div className="flex w-full min-h-screen">
+            <Sidebar setActivePage={setActivePage} activePage={activePage} />
+            <main className="flex-grow flex items-center justify-center p-4">
+              <Component {...pageProps} activePage={activePage} />
+            </main>
+          </div>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
